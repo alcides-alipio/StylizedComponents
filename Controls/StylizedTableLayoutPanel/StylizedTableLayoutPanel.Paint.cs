4a1effe41491fb1301f572a309dc65cfdbea326a
alcides-alipio <alcides.alipio.809@gmail.com>
2026-05-18 10:20:15 +0100

    feat(StylizedTableLayoutPanel): add StylizedTableLayoutPanel component with background transparent

--- Controls/StylizedTableLayoutPanel/StylizedTableLayoutPanel.Paint.cs
+++ Controls/StylizedTableLayoutPanel/StylizedTableLayoutPanel.Paint.cs
@@ -1,75 +1,14 @@
-﻿using StylizedComponents.Core;
+﻿using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 
 namespace StylizedComponents.Controls
 {
-    partial class StylizedTextBox
+    public partial class StylizedTableLayoutPanel
     {
         private Bitmap _backgroundCache = null;
-
-        protected override void OnPaintContent(PaintEventArgs e)
-        {
-            Graphics g = e.Graphics;
-
-            g.SmoothingMode = SmoothingMode.AntiAlias;
-            g.PixelOffsetMode = PixelOffsetMode.Half;
-            g.CompositingQuality = CompositingQuality.HighSpeed;
-
-            float half = BorderThickness / 2f;
-
-            RectangleF rect = new RectangleF(
-                half,
-                half,
-                Width - BorderThickness,
-                Height - BorderThickness
-            );
-
-            float radius = AutoRoundedCorners ? Utils.CalculateFullRoundBorderRadius(Width, Height) : BorderRadius;
-            float diameter = radius * 2f;
-
-            using (GraphicsPath path = new GraphicsPath())
-            {
-                if (radius <= 0)
-                {
-                    path.AddRectangle(rect);
-                }
-                else
-                {
-                    path.AddArc(rect.X, rect.Y, diameter, diameter, 180, 90);
-                    path.AddArc(rect.Right - diameter, rect.Y, diameter, diameter, 270, 90);
-                    path.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
-                    path.AddArc(rect.X, rect.Bottom - diameter, diameter, diameter, 90, 90);
-                }
-
-                path.CloseFigure();
-
-                using (SolidBrush brush = new SolidBrush(BackColor))
-                {
-                    g.FillPath(brush, path);
-                }
-
-                if (BorderThickness == 0)
-                    return;
-
-                Color borderColor = BorderColor;
-
-                if (_hoverState)
-                    borderColor = _hoverBorderColor;
-
-                if (_isFocused && borderColor != _hoverBorderColor)
-                    borderColor = _hoverBorderColor;
-
-                using (Pen pen = new Pen(borderColor, BorderThickness))
-                {
-                    pen.LineJoin = LineJoin.Round;
-                    pen.DashStyle = BorderStyle;
-
-                    g.DrawPath(pen, path);
-                }
-            }
-        }
 
         private Point GetControlRelativeOffset(Control control)
         {
@@ -138,7 +77,7 @@ namespace StylizedComponents.Controls
                 return;
             }
 
-            if (!_useTransparentBackground)
+            if (BackColor != Color.Transparent)
             {
                 base.OnPaintBackground(e);
                 return;
---
+++ Controls/StylizedTableLayoutPanel/StylizedTableLayoutPanel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace StylizedComponents.Controls
+{
+    [ToolboxItem(true)]
+    public partial class StylizedTableLayoutPanel : TableLayoutPanel
+    {
+        public StylizedTableLayoutPanel() : base() { }
+    }
+}
