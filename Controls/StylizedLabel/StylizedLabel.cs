9c333b8bab5f3d8da76ee53197f547282f5c5446
Alcides Alípio <alcides.alipio.809@gmail.com>
2026-05-19 14:37:36 +0100

    feat(StylizedLabel): add StylizedLabel component

---
+++ Controls/StylizedLabel/StylizedLabel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace StylizedComponents.Controls
+{
+    [ToolboxItem(true)]
+    [DesignerCategory("Code")]
+    public partial class StylizedLabel : Label
+    {
+        public StylizedLabel() : base() { }
+    }
+}
