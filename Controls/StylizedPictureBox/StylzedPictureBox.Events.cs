7177cece86f57908e94999dcb1e2f9c67b853d54
Alcides Alípio <alcides.alipio.809@gmail.com>
2026-06-22 20:23:39 +0100

    fix(StylizedPictureBox): fix transparent background update for location change

---
+++ Controls/StylizedPictureBox/StylzedPictureBox.Events.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StylizedComponents.Controls
+{
+    public partial class StylizedPictureBox
+    {
+        protected override void OnLocationChanged(EventArgs e)
+        {
+            _backgroundCache?.Dispose();
+            _backgroundCache = null;
+            base.OnLocationChanged(e);
+            Invalidate();
+        }
+    }
+}
